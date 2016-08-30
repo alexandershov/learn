c948af30092b56eb9e3dfba15450969792259fac
aershov <codumentary.com@gmail.com>
2016-08-30 22:08:58 +0300

    tcp slow start: initial commit

--- net/tcp-slow-start/README.md
+++ net/tcp-slow-start/README.md
@@ -4,14 +4,17 @@ Some code to check that TCP indeed has slow start
 
 ## How to run it?
 
-On remote host run:
+On `remote_host` run:
 
 ```shell
-$ ./server.sh
+$ ./server.sh 1234
 ```
 
 On local host run:
 
 ```shell
-$ docker-compose up
+$ TSS_HOST=remote_host TSS_PORT=1234 python client.py
 ```
+
+## Results
+![Tcp slow start](tcp-slow-start.png)
--- net/tcp-slow-start/client.py
+++ net/tcp-slow-start/client.py
@@ -1,10 +1,66 @@
+from collections import OrderedDict
+import asyncio
+import logging
 import os
+import time
+
+import matplotlib.pyplot as plt
+
+MAX_BYTES = 1 * 1024 * 1024  # 1 megabyte
+CHUNK_SIZE = 1024  # 1 kilobyte
 
 
 def main():
     host = _get_env('TSS_HOST')
     port = int(_get_env('TSS_PORT'))
-    print(host, port)
+    _configure_logging()
+    loop = asyncio.get_event_loop()
+    downloader = Downloader(
+        loop=loop,
+        host=host,
+        port=port,
+    )
+    loop.run_until_complete(downloader.download())
+    loop.close()
+
+
+def _configure_logging():
+    logging.basicConfig(level=logging.INFO)
+
+
+class Downloader:
+    def __init__(self, loop, host, port):
+        self.loop = loop
+        self.host = host
+        self.port = port
+        self.num_bytes = 0
+        self.stats = OrderedDict()  # time -> speed
+        self.polling_interval = 0.01
+        self.started_at = None
+
+    async def download(self):
+        reader, writer = await asyncio.open_connection(
+            host=self.host,
+            port=self.port,
+            loop=self.loop,
+        )
+        self.started_at = time.time()
+        self.loop.call_soon(self.save_stats)
+        while self.num_bytes < MAX_BYTES:
+            data = await reader.read(CHUNK_SIZE)
+            self.num_bytes += len(data)
+        self.plot_stats()
+
+    def save_stats(self):
+        duration = time.time() - self.started_at
+        self.stats[duration] = self.num_bytes / duration
+        self.loop.call_later(self.polling_interval, self.save_stats)
+
+    def plot_stats(self):
+        plt.plot(list(self.stats.keys()), list(self.stats.values()))
+        plt.xlabel('time (seconds)')
+        plt.ylabel('speed (bytes/second)')
+        plt.show()
 
 
 def _get_env(name):
