de76abd3f75d1b38146f6370b2a127ca6c6a2426
aershov <codumentary.com@gmail.com>
2016-09-04 18:23:33 +0300

    os: grab mutex before printing

--- os/synchr/README.md
+++ os/synchr/README.md
@@ -3,3 +3,7 @@ Some code to check pthreads, mutexes, semaphores and condition variables.
 
 ## How to run it?
 Open the project in CLion.
+
+## Lessons learned
+Functions with `_np` suffix in <pthreads.h> are not portable. NP - Not
+Portable.
--- os/synchr/main.cpp
+++ os/synchr/main.cpp
@@ -1,4 +1,5 @@
 #include <pthread.h>
+#include <semaphore.h>
 
 #include <iostream>
 #include <vector>
@@ -7,25 +8,50 @@
 const int NUM_THREADS = 10;
 
 
+struct synchr_data_t {
+    pthread_mutex_t *mutex;
+    sem_t *semaphore;
+};
+
+
 void *do_stuff(void *data) {
+    synchr_data_t *s_data = (synchr_data_t*)data;
     auto thread = pthread_self();
     // pthread_mach_thread_np is Mac OS X specific
     auto thread_id = pthread_mach_thread_np(thread);
+
+    pthread_mutex_lock(s_data->mutex);
     std::cout << "my id = " << thread_id << std::endl;
+    pthread_mutex_unlock(s_data->mutex);
     return nullptr;
 }
 
-int main() {
-    do_stuff(nullptr);
-    std::vector<pthread_t*> all_threads;
+std::vector<pthread_t> create_threads(uint32_t n, synchr_data_t *data) {
+    std::vector<pthread_t> all_threads(n);
     for (uint32_t i = 0; i < NUM_THREADS; i++) {
         pthread_t thread;
-        pthread_create(&thread, nullptr, do_stuff, nullptr);
-        all_threads.push_back(&thread);
+        pthread_create(&thread, nullptr, do_stuff, data);
+        all_threads.push_back(thread);
     }
-    for (pthread_t *thread: all_threads) {
-        pthread_join(*thread, nullptr);
+    return all_threads;
+}
+
+void join_threads(std::vector<pthread_t> all_threads) {
+    for (pthread_t thread: all_threads) {
+        pthread_join(thread, nullptr);
     }
 }
+
+int main() {
+    synchr_data_t data;
+    pthread_mutex_t mutex;
+    pthread_mutex_init(&mutex, nullptr);
+    sem_t semaphore;
+    data.mutex = &mutex;
+    auto all_threads = create_threads(NUM_THREADS, &data);
+    join_threads(all_threads);
+}
+
+
 
 
