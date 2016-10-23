975ca0ea84f7c6bf4d958f8eb92d018342d7f0be
aershov <codumentary.com@gmail.com>
2016-10-23 16:17:03 +0300

    pearls: fix right shift tests
    Take into account that right shift on a number of bits that is
    greater than the size of left operand is undefined.

--- pearls/tests/ch1/test_ex2.cpp
+++ pearls/tests/ch1/test_ex2.cpp
@@ -8,12 +8,14 @@ class BitOpsRightShiftTest : public ::testing::TestWithParam<size_t> {
 
 TEST_P(BitOpsRightShiftTest, ItCanRightShiftIntegers) {
     auto num_bits = GetParam();
-    EXPECT_EQ(ch1::RightShift(1, num_bits), std::size_t{1} << num_bits);
+    size_t expected_result = 0;
+    if (num_bits < (CHAR_BIT * sizeof(std::size_t))) {
+        expected_result = std::size_t{1} << num_bits;
+    }
+    EXPECT_EQ(ch1::RightShift(1, num_bits), expected_result);
 }
 
 INSTANTIATE_TEST_CASE_P(
         InterestingShifts,
         BitOpsRightShiftTest,
-        ::testing::Values(0, 1, 3, 63, 64, 128));
-
-
+        ::testing::Values(0, 1, 3, 63, 64, 65, 128));
