1d5438bba51fa288cff5a5d7853ada684252f051
aershov <codumentary.com@gmail.com>
2016-10-23 16:35:36 +0300

    pearls: extract left shift function

--- pearls/pearls/bitops.cpp
+++ pearls/pearls/bitops.cpp
@@ -11,11 +11,15 @@ size_t ch1::RightShift(size_t x, size_t num_bits) {
     return shifted_x;
 }
 
-bool ::ch1::IsSet(size_t x, size_t index) {
+size_t LeftShift(size_t x, size_t num_bits) {
     size_t shifted_x = x;
-    while (index > 0) {
+    while (num_bits > 0) {
         shifted_x /= 2;
-        index--;
+        num_bits--;
     }
-    return (shifted_x % 2) != 0;
+    return shifted_x;
+}
+
+bool ::ch1::IsSet(size_t x, size_t index) {
+    return (LeftShift(x, index) % 2) != 0;
 }
