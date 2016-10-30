972e51b274e507e2ea8d12bd17be7b7e3a7df849
aershov <codumentary.com@gmail.com>
2016-10-30 22:05:23 +0300

    pearls: compare cpp sort and bitvector sort

--- pearls/tests/ch1/test_ex3.cpp
+++ pearls/tests/ch1/test_ex3.cpp
@@ -1,7 +1,7 @@
+#include <NumbersGenerator.h>
 #include "gtest/gtest.h"
 #include "Bitvector.h"
 #include "bitvector_sort.h"
-
 
 class BitvectorTest : public ::testing::TestWithParam<size_t> {
 
@@ -32,6 +32,31 @@ TEST(SortTest, ItSortsIterator) {
     std::vector<size_t> sorted_numbers = ch1::bitvector_sort(numbers);
     std::vector<size_t> expected_sorted_numbers{1, 2, 4, 8, 9};
     EXPECT_EQ(sorted_numbers, expected_sorted_numbers);
+}
+
+
+std::vector<size_t> _generate_numbers(size_t max_value, size_t num_numbers) {
+    ch1::NumbersGenerator generator(0, max_value, num_numbers);
+    std::vector<size_t> numbers;
+    while (!generator.IsFinished()) {
+        numbers.push_back(generator.GetNextNumber());
+    }
+    return numbers;
+}
+
+
+std::vector<size_t> _cpp_sort(const std::vector<size_t> &numbers) {
+    std::vector<size_t> numbers_copy(numbers);
+    std::sort(numbers_copy.begin(), numbers_copy.end());
+    return numbers_copy;
+}
+
+TEST(SortTest, CheckPerformance) {
+    std::vector<size_t> numbers = _generate_numbers(28000, 27000);
+    EXPECT_EQ(numbers.size(), 27000);
+    std::vector<size_t> bitvector_sorted_numbers = ch1::bitvector_sort(numbers);
+    std::vector<size_t> cpp_sorted_numbers = _cpp_sort(numbers);
+    EXPECT_EQ(bitvector_sorted_numbers, cpp_sorted_numbers);
 }
 
 INSTANTIATE_TEST_CASE_P(
