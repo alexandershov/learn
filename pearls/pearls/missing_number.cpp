46888b7729b03460cc2c74c78ac9e966321a225f
aershov <codumentary.com@gmail.com>
2016-11-01 21:31:27 +0300

    pearls: test missing_number edge cases

--- pearls/pearls/missing_number.cpp
+++ pearls/pearls/missing_number.cpp
@@ -13,19 +13,17 @@ bool _range_is_full(const std::vector<size_t> &numbers, size_t begin, size_t end
 }
 
 ssize_t ch2::get_missing_number(std::vector<size_t> &numbers) {
-    assert(numbers.size() > 0);
     std::sort(numbers.begin(), numbers.end());
     size_t begin = 0;
     size_t end = numbers.size();
     while (!_range_is_full(numbers, begin, end)) {
         if (end - begin == 2) {
-            if (numbers[begin + 1] - numbers[begin] > 1) {
-                return numbers[begin] + 1;
-            }
+            return numbers[begin] + 1;
         }
         size_t i = (begin + end) / 2;
+//      end is decreasing, because the only case when i + 1 == end is when size is 2 and we handle
+//      this case
         if (!_range_is_full(numbers, begin, i + 1)) {
-//            TODO: hmm, looks suspicious, is end decreasing here?
             end = i + 1;
         } else if (!_range_is_full(numbers, i, end)) {
             begin = i;
--- pearls/tests/ch2/test_missing_number.cpp
+++ pearls/tests/ch2/test_missing_number.cpp
@@ -3,12 +3,28 @@
 
 
 TEST(MissingNumber, ItFindsMissingNumber) {
-    std::vector<size_t> numbers = {3, 4, 1, 5};
+    std::vector<size_t> numbers{3, 4, 1, 5};
     EXPECT_EQ(ch2::get_missing_number(numbers), 2);
 }
 
 
 TEST(MissingNumber, ItHandlesNoMissing) {
-    std::vector<size_t> numbers = {3, 4, 2, 1, 5};
+    std::vector<size_t> numbers{3, 4, 2, 1, 5};
+    EXPECT_EQ(ch2::get_missing_number(numbers), -1);
+}
+
+
+TEST(MissingNumber, ItWorksOnTwoConsecutiveNumbers) {
+    std::vector<size_t> numbers{0, 1};
+    EXPECT_EQ(ch2::get_missing_number(numbers), -1);
+}
+
+TEST(MissingNumber, ItWorksOnOneNumber) {
+    std::vector<size_t> numbers{0};
+    EXPECT_EQ(ch2::get_missing_number(numbers), -1);
+}
+
+TEST(MissingNumber, ItWorksOnNoNumber) {
+    std::vector<size_t> numbers{};
     EXPECT_EQ(ch2::get_missing_number(numbers), -1);
 }
