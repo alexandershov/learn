80d2cbf62a41710da27dd40025bce46713097dea
aershov <codumentary.com@gmail.com>
2017-02-05 14:08:48 +0300

    describe noatime

--- os/files/src/main.cpp
+++ os/files/src/main.cpp
@@ -6,8 +6,12 @@ int main() {
   struct stat file_stat;
   stat("../src/main.cpp", &file_stat);
   std::cerr << "ino = " << file_stat.st_ino
+    // change time, changed when either inode or contents changes
 	    << ", ctime = " << file_stat.st_ctime
+    // access time, can be changed by `touch`. Usually doesn't change when you read a file (as an optimization).
+    // You can bypass this optimization by changing noatime to atime in /etc/fstab and remounting
 	    << ", atime = " << file_stat.st_atime
+    // modification time, changed when content changes
 	    << ", mtime = " << file_stat.st_mtime
 	    << "\n";
 }
