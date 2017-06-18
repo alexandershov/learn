aea878ffbfb25833a4f88a17826c80a31f65a75f
aershov <codumentary.com@gmail.com>
2017-06-18 19:14:30 +0300

    add simple event loop

--- python/asio.py
+++ python/asio.py
@@ -1,4 +1,9 @@
+from collections import namedtuple
+import datetime
 import heapq
+import logging
+import time
+
 
 class Task:
     def __init__(self):
@@ -12,16 +17,30 @@ class Task:
             value = callback(value)
         return value
 
+    
+Event = namedtuple('Event', ['call_time', 'fn'])
+
 
 class EventLoop:
     def __init__(self):
         self._events = []
     
     def call_later(self, delay, fn):
-        pass
+        call_time = datetime.datetime.utcnow() + datetime.timedelta(seconds=delay)
+        event = Event(call_time, fn)
+        heapq.heappush(self._events, event)
     
     def start(self):
-        pass
+        while self._events:
+            first = self._events[0]
+            now = datetime.datetime.utcnow()
+            if first.call_time <= now:
+                event = heapq.heappop(self._events)
+                event.fn()
+            else:
+                sleep_duration = (first.call_time - now).total_seconds()
+                logging.info('sleeping for %s', sleep_duration)
+                time.sleep(sleep_duration)
 
     
 def test_task():
@@ -35,6 +54,8 @@ def test_task():
 def test_event_loop():
     values = []
     loop = EventLoop()
-    loop.call_later(1, lambda: values.append(3))
+    loop.call_later(0.3, lambda: values.append(3))
+    loop.call_later(0.2, lambda: values.append(2))
+    loop.call_later(0.4, lambda: values.append(4))
     loop.start()
-    assert values == [3]
+    assert values == [2, 3, 4]
