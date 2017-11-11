e0fce920b6d09b265293fb92eec67201d3401ab9
aershov <codumentary.com@gmail.com>
2017-11-11 10:10:53 +0300

    use checked_lock & checked_unlock

--- os/synchr/condvar.cpp
+++ os/synchr/condvar.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include <pthread.h>
 
@@ -6,14 +7,22 @@ pthread_mutex_t mutex = PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP;
 pthread_cond_t condvar = PTHREAD_COND_INITIALIZER;
 int child_exited = 0;
 
+void checked_lock(pthread_mutex_t* mutex, const std::string& description) {
+  auto result = pthread_mutex_lock(mutex);
+  std::cerr << description << " lock result " << result << "\n";
+}
+
+void checked_unlock(pthread_mutex_t* mutex, const std::string& description) {
+  auto result = pthread_mutex_unlock(mutex);
+  std::cerr << description << " unlock result " << result << "\n";
+}
+
 void signal_exit() {
-  auto result = pthread_mutex_unlock(&mutex);
-  std::cerr << "unlock in child " << result << "\n";
+  checked_unlock(&mutex, "lock in child");
 }
 
 void my_join() {
-  auto result = pthread_mutex_lock(&mutex);
-  std::cerr << "lock in join " << result << "\n";
+  checked_lock(&mutex, "lock in join");
 }
 
 void* child_thread(void* data) {
@@ -24,7 +33,7 @@ void* child_thread(void* data) {
 
 int main() {
   pthread_t child;
-  std::cerr << "initial lock " << pthread_mutex_lock(&mutex) << "\n";
+  checked_lock(&mutex, "initial lock");
   pthread_create(&child, nullptr, child_thread, nullptr);
   my_join();
   std::cerr << "parent done\n";
